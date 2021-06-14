eeb85ea1bd6f22a0494dc9d2f53142c9ae0c7be3
akashsaini-nagarro <akash.saini@nagarro.com>
2021-06-14 23:44:18 +0530

    updating JenkinsFile

--- Build Trigger.ps1
+++ Build Trigger.ps1
@@ -1,7 +1,7 @@
 $user = 'test'
 $token = '11e30f52613642a96d350c04a6558f834e'
 $prevcommit = '04cc6a077bbf42b17068c6d762515c3bde36ab33'
-$latestcommit = '08d372de0f4f033981e582459636eb50f45df9b4'
+$latestcommit = '1ae4529c5867dd3c1c12e3aa5b5aa8341887c30b'
 
 # The header is the username and token concatenated together
 $pair = "$($user):$($token)"
